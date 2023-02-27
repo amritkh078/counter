b2d06ee22402d41bbb0225cc87ba4b7593a1abeb
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-27 21:14:58 +0545

    counter

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -8,13 +8,9 @@ const Counter = () => {
         <h1>Count: {count}</h1>
       </div>
       <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increment
-        </button>
-        <button onClick={() => setCount((count) => count - 1)}>
-          Decrement
-        </button>
-        <button onClick={() => setCount(0)}>Reset</button>
+        <button  onClick={() => setCount((count) => count + 1)}>Increment</button>
+        <button  onClick={() => setCount((count) => count - 1)}>Decrement</button>
+        <button  onClick={() => setCount(0)}>Reset</button>
       </div>
     </div>
   );
