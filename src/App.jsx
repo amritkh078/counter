107680030fc3fcf4bd101bbb44a49522b03023d6
amritkh078 <amritkhatiwada078@gmail.com>
2023-02-27 20:36:10 +0545

    use tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Counter from './component/Counter'
+import React from "react";
+import Counter from "./component/Counter";
 
 const App = () => {
   return (
     <div>
       <Counter />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
